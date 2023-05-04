e526d47ca051cc68577d0f5e15f7fc83de84796a
Vladislav K <vladislav.k@smartym.pro>
2023-05-04 17:51:02 +0300

    hello uri

--- src/main/java/com/example/renderhello/Controller.java
+++ src/main/java/com/example/renderhello/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
 
-    @GetMapping
+    @GetMapping("hello")
     public void hello() {
         System.out.println("hello");
     }
